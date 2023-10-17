21a3c2da1939601c1c37baaf66e040722f6a909e
Hu Bowen <contact@hubowen.dev>
2023-10-17 20:16:46 +0800

    I forgor about part c & d

--- Week 02 - Introduction to C# (Part 2)/S10255800_Qn01/S10255800_Qn01/Program.cs
+++ Week 02 - Introduction to C# (Part 2)/S10255800_Qn01/S10255800_Qn01/Program.cs
@@ -3,7 +3,10 @@
 // Date: 17 Oct 2023
 
 /* Part A */
-#region Part A 
+#region Part A
+
+using System.Collections;
+
 Console.WriteLine("===== Part A =====");
 
 var arr = new int[5];
@@ -67,6 +70,54 @@ while (true)
 // Display contents of list
 Console.Write("Contents of list: ");
 foreach (var i in list)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine("\n==================");
+#endregion
+
+/* Part C */
+#region Part C
+Console.WriteLine("\n===== Part C =====");
+
+// Initialise stack & Push values
+Stack<int> stack = new Stack<int>(list);
+stack.Push(56);
+stack.Push(72);
+
+// Pop stack & Display contents
+Console.Write("The stack contains: ");
+while (stack.Count > 0)
+{
+    Console.Write(stack.Pop() + " ");
+}
+
+Console.WriteLine("\n==================");
+#endregion
+
+/* Part D */
+#region Part D
+Console.WriteLine("\n===== Part D =====");
+
+// Initialise queue with contents of list
+// & enqueue values
+Queue<int> queue = new Queue<int>(list);
+queue.Enqueue(23);
+queue.Enqueue(45);
+
+// Display the contents of the enqueued queue
+Console.Write("Content of queue after enqueuing: ");
+foreach (var i in queue)
+{
+    Console.Write(i + " ");
+}
+
+// Dequeue the queue & display
+queue.Dequeue();
+
+Console.Write("\nContent of queue after dequeuing: ");
+foreach (var i in queue)
 {
     Console.Write(i + " ");
 }
