bd706663cb2544a12b84575707c0f952aa93d4c2
Hu Bowen <contact@hubowen.dev>
2023-11-18 22:26:16 +0800

    Added PRG2 practical test 1 revision

---
+++ Assessments/Sample Practical Test/PhoneApp_S10255800/PhoneApp_S10255800/Phone.cs
@@ -0,0 +1,46 @@
+namespace PhoneApp_S10255800;
+
+public class Phone
+{
+    public string PhoneNum { get; }
+    public int Usage { get; set; }
+    public string PlanType { get; }
+
+    public Phone()
+    {
+        
+    }
+
+    public Phone(string phoneNum, int usage, string planType)
+    {
+        PhoneNum = phoneNum;
+        Usage = usage;
+        PlanType = planType;
+    }
+
+    public double CalculateCharge()
+    {
+        int threshold = 0;
+        double connRate = 0.5;
+
+        switch (PlanType)
+        {
+            case "B":
+                threshold = 1000;
+                connRate = 0.2;
+                break;
+            case "C":
+                threshold = 5000;
+                connRate = 0.1;
+                break;
+        }
+
+        int finalUsage = Math.Max(Usage - threshold, 0);
+        return (finalUsage * connRate) / 100;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ Assessments/Sample Practical Test/PhoneApp_S10255800/PhoneApp_S10255800/Program.cs
@@ -0,0 +1,59 @@
+﻿// Phone App
+// Written by: Hu Bowen
+// Date: 18 Nov 2023
+
+using PhoneApp_S10255800;
+
+// Part A
+List<Phone> phones = new List<Phone>();
+
+// Part B
+void InitialisePhones()
+{
+    using (StreamReader sr = File.OpenText("PhoneDetails.csv"))
+    {
+        string? line = sr.ReadLine();
+        while ((line = sr.ReadLine()) != null)
+        {
+            string[] data = line.Split(',');
+            phones.Add(new Phone(data[0], int.Parse(data[1]), data[2]));
+        }
+    }
+}
+
+InitialisePhones();
+
+// Part C
+void DisplayOutput()
+{
+    Console.WriteLine("{0, -10} {1, -10} {2, -10} {3}", "PhoneNo", "Usage", "PlanType", "PhoneCharge($)");
+    foreach (Phone phone in phones)
+    {
+        Console.WriteLine("{0, -10} {1, -10} {2, -10} {3,14:0.00}", phone.PhoneNum, phone.Usage, phone.PlanType, Math.Round(phone.CalculateCharge(), 2));
+    }
+}
+
+DisplayOutput();
+
+// Part D
+Console.WriteLine("\nUpdate Phone Usage");
+Console.WriteLine("----------------");
+
+Console.Write("Enter phone no: ");
+string phoneNumber = Console.ReadLine();
+
+Phone? searchedNumber = phones.FirstOrDefault(phone => phone.PhoneNum == phoneNumber);
+
+if (searchedNumber == null)
+{
+    Console.WriteLine("Phone not found!");
+    return;
+}
+
+Console.WriteLine("Current Usage: " + searchedNumber.Usage);
+Console.Write("Enter new usage: ");
+
+searchedNumber.Usage = int.Parse(Console.ReadLine());
+Console.WriteLine("The new usage is updated successfully\n");
+
+DisplayOutput();
